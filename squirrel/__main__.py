188bfbfd47fb53c0b524f378e11783e0f7755f95
ekardnam <lucabertozzi.pub@gmail.com>
2019-02-06 10:16:15 +0000

    More commands for array manipulation

--- squirrel/__main__.py
+++ squirrel/__main__.py
@@ -1,8 +1,21 @@
-from data import liber_primus
+from data import liber_primus, table
 from termcolor import colored
-from routines import rune_to_text, text_to_rune, rune_to_gematria
+from routines import rune_to_text, text_to_rune, rune_to_gematria, rune_frequency
 import numpy
+import matplotlib.pyplot as plt
 
+
+class Routine:
+	def __init__(self, commands):
+		self.commands = commands
+
+	def exec(self, squirrel):
+		for command in self.commands:
+			ret = squirrel.eval(command)
+		return ret
+
+	def __str__(self):
+		return self.commands.__str__()
 
 """
 
@@ -51,7 +64,7 @@ class Squirrel:
 		return self.variables
 
 	def prompt(self):
-		expr = input(colored("squirrel >", "red"))
+		expr = input(colored("squirrel> ", "red"))
 		return expr
 
 	def eval(self, expr):
@@ -62,8 +75,8 @@ class Squirrel:
 		self.running = False
 
 	def eval_parts(self, parts):
-		if parts[0] in self.command_handlers:
-			return self.command_handlers[parts[0]](self, parts[1:])
+		if parts[0].strip() in self.command_handlers:
+			return self.command_handlers[parts[0].strip()](self, parts[1:])
 		else:
 			raise RuntimeError("Invalid command issued")
 
@@ -160,20 +173,111 @@ def gematria_command(squirrel, args):
 
 @squirrel.register_command("rotate")
 def rotate_command(squirrel, args):
-	expect_full(args, "Args expected")
-	rotation = 0
-	try:
-		rotation = int(args[0])
-	except ValueError as e:
-		raise RuntimeError("Invalid rotation") from e
-	gematria = squirrel.eval_parts(args[1:])
-	return (gematria + rotation) % 29
+	if len(args) < 2:
+		raise RuntimeError("args expected")
+	rotation = squirrel.get_variable(args[0])
+	if type(rotation) != int:
+		raise RuntimeError("integer expected")
+	array = squirrel.eval_parts(args[1:])
+	return (array + rotation) % 29
 
-@squirrel.register_command("invert")
-def invert_command(squirrel, args):
+@squirrel.register_command("reverse")
+def reverse_command(squirrel, args):
 	expect_full(args, "Args expected")
 	gematria = squirrel.eval_parts(args)
 	return (28 - gematria) % 29
+
+@squirrel.register_command("runefreq")
+def runefreq_command(squirrel, args):
+	expect_full(args, "args expected")
+	runes = squirrel.eval_parts(args)
+	return rune_frequency(runes)
+
+@squirrel.register_command("plot")
+def plot_command(squirrel, args):
+	expect_full(args, "args expected")
+	dicto = squirrel.eval_parts(args)
+	plt.bar([table[rune] for rune in list(dicto.keys())], dicto.values(), color='g')
+
+@squirrel.register_command("showplot")
+def showplot_command(squirrel, args):
+	plt.show()
+
+@squirrel.register_command("saveplot")
+def saveplot_command(squirrel, args):
+	expect_full(args, "args expected")
+	plt.savefig(str(squirrel.get_variable(args[0])))
+
+@squirrel.register_command("routine")
+def routine_command(squirrel, args):
+	expect_full(args, "args expected")
+	commands = [c.strip() for c in " ".join(args).split(";")]
+	return Routine(commands)
+
+@squirrel.register_command("run")
+def run_command(squirrel, args):
+	expect_full(args, "args expected")
+	routine = squirrel.get_variable(args[0])
+	if type(routine) == Routine:
+		return routine.exec(squirrel)
+	else:
+		raise RuntimeError("Routine expected")
+
+@squirrel.register_command("chapterwise")
+def chapterwise_command(squirrel, args):
+	expect_full(args, "args expected")
+	for chapter in range(0, 57):
+		squirrel.eval("set chapter int {}".format(chapter))
+		squirrel.eval("run {}".format(args[0]))
+
+@squirrel.register_command("closeplot")
+def closeplot_command(squirrel, args):
+	plt.close()
+
+@squirrel.register_command("concat")
+def concat_command(squirrel, args):
+	if len(args) < 2:
+		raise RuntimeError("args expected")
+	return str(squirrel.get_variable(args[0])) + str(squirrel.eval_parts(args[1:]))
+
+@squirrel.register_command("array")
+def array_command(squirrel, args):
+	if len(args) <= 0:
+		raise RuntimeError("args expected")
+	try:
+		return numpy.array([int(num.strip()) for num in " ".join(args).split(",")])
+	except ValueError as e:
+		raise RuntimeError("int expected") from e
+
+@squirrel.register_command("sum")
+def sum_command(squirrel, args):
+	if len(args) < 2:
+		raise RuntimeError("args expected")
+	result = squirrel.get_variable(args[0])
+	if type(result) != numpy.ndarray:
+		raise RuntimeError("array expected")
+	result = numpy.copy(result)
+	for var in args[1:]:
+		array = squirrel.get_variable(var)
+		if type(array) != numpy.ndarray:
+			raise RuntimeError("array expected")
+		if array.shape != result.shape:
+			raise RunetimeError("same shape required")
+		result += array
+		result %= 29
+	return result
+
+@squirrel.register_command("mod")
+def mod_command(squirrel, args):
+	if len(args) < 2:
+		raise RuntimeError("args expected")
+	modulus = squirrel.get_variable(args[0])
+	if type(modulus) != int:
+		raise RuntimeError("int expected")
+	array = squirrel.eval_parts(args[1:])
+	if type(array) != numpy.ndarray:
+		raise RuntimeError("array expected")
+	return array % modulus
 
 if __name__ == "__main__":
 	squirrel.serve()
--- squirrel/routines.py
+++ squirrel/routines.py
@@ -36,3 +36,40 @@ def rune_to_gematria(runes):
 		if rune in gematria_values:
 			result.append(gematria_values[rune])
 	return result
+
+def rune_frequency(runes):
+	freqs = {
+		"ᚠ": 0, 
+		"ᚢ": 0,
+		"ᚦ": 0,
+		"ᚩ": 0,
+		"ᚱ": 0,
+		"ᚳ": 0,
+		"ᚷ": 0,
+		"ᚹ": 0,
+		"ᚻ": 0,
+		"ᚾ": 0,
+		"ᛁ": 0,
+		"ᛂ": 0,
+		"ᛇ": 0,
+		"ᛈ": 0,
+		"ᛉ": 0,
+		"ᛋ": 0,
+		"ᛏ": 0,
+		"ᛒ": 0,
+		"ᛖ": 0,
+		"ᛗ": 0,
+		"ᛚ": 0,
+		"ᛝ": 0,
+		"ᛟ": 0,
+		"ᛞ": 0,
+		"ᚪ": 0,
+		"ᚫ": 0,
+		"ᚣ": 0,
+		"ᛡ": 0,
+		"ᛠ": 0
+	}
+	for rune in runes:
+		if rune in freqs:
+			freqs[rune] += 1
+	return freqs
